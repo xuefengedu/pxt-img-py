5e01aa9a7b6fe32921015a0db951ac8e4560942f
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-10-18 02:35:22 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
 Echo go to dir: "%~dp0"
-C:\Users\scxsvc\AppData\Local\Programs\Python\Python37\python test.py -i dt.c -o output.txt
+C:\Python37\python test.py -i dt.c -o output.txt
 pause
